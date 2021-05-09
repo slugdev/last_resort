85dd7d2bf2e195df6d4177776657e8f4a63a587e
Sam <slugdev@gmail.com>
2021-05-09 08:05:46 -0500

    Add files via upload

---
+++ last_resort/last_resort.cpp
@@ -0,0 +1,12 @@
+﻿// last_resort.cpp : Defines the entry point for the application.
+//
+
+#include "last_resort.h"
+
+bool last_resort(float* nodes_in, int node_cnt, int* tris_in)
+
+int main()
+{
+
+	return 0;
+}
